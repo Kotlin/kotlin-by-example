dac26bdaace1579cae80118abb26f472ee9c8d72
Jonathan Hult <jonathan@jonathanhult.com>
2018-01-08 16:10:43 -0500

    Use proper jekyll repo

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -v `pwd`:/tmp -p 4000:4000 hhariri/jekyll jekyll serve -s /tmp --host 0.0.0.0 --incremental
+docker run -v `pwd`:/tmp -p 4000:4000 jekyll/jekyll jekyll serve -s /tmp --host 0.0.0.0 --incremental
