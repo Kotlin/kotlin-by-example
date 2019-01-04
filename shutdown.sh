a8eda8ff0e5fc0179d6ea4b9dfe2af865d351e90
deeper-x <albertodeprezzo@gmail.com>
2019-01-04 18:03:24 +0100

    added a docker/jekyll shutdown script

---
+++ shutdown.sh
@@ -0,0 +1,26 @@
+TO_STOP="jekyll"
+
+# METHOD 1: search for jekyll process and kill. 
+# ps aux | grep ${TO_STOP} | awk '{print $2}' | xargs kill -9 2>/dev/null
+# END METHOD 1
+
+# METHOD 2: search for the container ID and kill it. 
+CONTAINER=$( docker container ls  | grep $TO_STOP | cut -d" " -f1 )
+CLENGTH=${#CONTAINER}
+STATUS="successfully"
+
+if [[ $CLENGTH -gt 0 ]];then 
+	docker container kill $CONTAINER 1>/dev/null
+else
+	STATUS="already"
+fi
+# END METHOD 2
+
+# RESULT: get a log string 
+# NOTE: exit status 123 is related to method one, when grep search for ruby and other bin stuff 
+if [[ $? -eq 123 || $? -eq 0 ]];then 
+        echo "${TO_STOP} ${STATUS} stopped...";
+else
+	echo "Error: something went wrong!"	
+fi
+
